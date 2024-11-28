e7b2636a47e5ae46dc1526113a74028da02d8eb2
SicaDeveloper <rajdangol.dev@gmail.com>
2024-11-28 09:05:39 +0545

    changes

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/nursesNearMe')
+
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req,res) => {
     res.render('index.ejs')
 })
+
 
 app.listen(3000,()=>{
     console.log("Connected")
