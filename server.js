3c73696765f12190818b8c0f913ed4ecb15e4a93
SicaDeveloper <rajdangol.dev@gmail.com>
2024-11-28 09:17:13 +0545

    changes and more setup

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
+var path = require('path');
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/nursesNearMe')
 
+app.use(express.static((path.join(__dirname + 'public'))))
+
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 
-app.set('view engine', 'ejs')
 
 app.get('/', (req,res) => {
     res.render('index.ejs')
